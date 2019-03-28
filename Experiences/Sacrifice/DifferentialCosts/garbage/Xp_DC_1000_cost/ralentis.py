47e73dd75ef5f33f64d5b2d2facfde0acfb0e326
jlie10 <julien.lie@protonmail.com>
2019-03-28 11:43:36 +0100

    Adding only condensed result files bcz size

--- Experiences/Sacrifice/DifferentialCosts/garbage/Xp_DC_1000_cost/ralentis.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env pythonimport random
-import math
-for ii in range(int(random.random()*20000000)):
-    jj = math.sqrt(ii)
--- Experiences/Sacrifice/DifferentialCosts/garbage/Xp_DC_200_context/multi.sh
+++
@@ -1,7 +0,0 @@
-
-
-for ((i=0 ; 10 - $i ; i++))
-    do echo lancement de lexperience $i
-    nohup nice sh ./Explore.sh $1.evo $1.evm $1.py &
-    sleep 2
-done
